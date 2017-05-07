39f67b6c06254157e18d34126afbba2ce9bc6cd6
Opsarde <Opsarde@users.noreply.github.com>
2017-05-07 16:05:43 -0700

    Modified Main to suit newest project

--- src/opengg/Main.java
+++ src/opengg/Main.java
@@ -1,21 +1,103 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************
+ *     file: Main.java
+ *     author: 
+ *     class: CS 445 - Computer Graphics
+ * 
+ *     assignment: program 3
+ *     last modified: 5/9/2017
+ * 
+ * 
+ *************************************************************/
 package opengg;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.input.Keyboard;
+
+import java.util.List;
 
 /**
  *
- * @author shun7817
+ * @author Shun Lu
  */
 public class Main {
+    /**
+     * METHOD: start
+     * PURPOSE: method to start GL window and render
+     */
+    public void start() {
+        try {
+            createWindow();
+            createKeyboard();
+            initGL();
+            render();
+        } catch (Exception e) {
+            // what is the error
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * METHOD: createWindow
+     * PURPOSE: method to create display object 
+     */
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        Display.setDisplayMode(new DisplayMode(640, 480));
+        Display.setTitle("Simple Graphic Rendering");
+        Display.create();
+    }
+    
+    /**
+     * METHOD: createKeyboard
+     * PURPOSE: method to create keyboard object 
+     */
+    private void createKeyboard() throws Exception {
+        Keyboard.create();
+    }
 
     /**
+     * METHOD: initGL
+     * PURPOSE: method that initialize projection matrix on window 
+     */
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-640 / 2, 640 / 2, -480 / 2, 480 / 2, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+    
+    /**
+     * METHOD: render
+     * PURPOSE: render every frame until window is closed or press escape key
+     */
+    private void render() {
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            try {
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glLoadIdentity();
+                // TODO:
+                // Render cube here
+                Display.update();
+                Display.sync(60);
+            } catch (Exception e) {
+            }
+        }
+        Display.destroy();
+        Keyboard.destroy();
+    }
+    
+    /**
+     * METHOD: main
+     * PURPOSE: run the OpenGL program
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Main main = new Main();
+        main.start();
     }
-    
 }
