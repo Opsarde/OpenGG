2e87faf98681f4eece5d4af90ac2e441b09b30bd
Tuan Pham <tuandpham75@gmail.com>
2017-05-17 21:24:05 -0700

    Chunk and Block class

--- src/opengg/Vector3Float.java
+++ src/opengg/Vector3Float.java
@@ -1,24 +1,16 @@
-/*************************************************************
- *     file: Vector3Float.java
- *     authors: OpenGG (Shun Lu, Roenyl Tisoy, Tuan Pham, Evan Gunell)
- *     class: CS 445 - Computer Graphics
- * 
- *     assignment: program 3
- *     last modified: 5/9/2017
- * 
- *     purpose: This class represents a 3D vector
- *     using three single-precision floating-point numbers.
- * 
- *************************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opengg;
 
-
+/**
+ *
+ * @author shun7817
+ */
 public class Vector3Float {
     public float x, y, z;
-    
-    /**
-     * METHOD: constructor
-     * PURPOSE: creates a Vector3Float object with x, y, and z parameters
-     */
     public Vector3Float(float x, float y, float z) {
         this.x = x;
         this.y = y;
